1ecd584c6542030c3036229b46c7df4278ce7086
Ivan Strekin <grivoosgarm@gmail.com>
2025-11-03 18:06:12 +0300

    Начал писать js.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-let tasks_block;
+let tasks = [];
+const button = document.querySelector('#Add_Button');
+const title = document.querySelector('#title');
+const filterSelect = document.querySelector('#filter');
+
+function clearTaskContainers() {
+    const blocks_tasks = document.querySelectorAll('.Tasks_block');
+    blocks_tasks.forEach(block => {
+        if (block) {
+            block.innerHTML = '';
+        }
+    });
+}
+
+function renderTasks() {
+    const notDoneContainer = document.querySelector('.not_done_tasks');
+    const inProcessContainer = document.querySelector('.in_process_tasks');
+    const doneContainer = document.querySelector('.done_tasks');
+    clearTaskContainers(notDoneContainer, inProcessContainer, doneContainer);
+}
+
+button.addEventListener("click", () => {
+    const title_text = title.value;
+    if (title_text !== "") {
+        tasks.push([title_text, 0]);
+    }
+    title.value = "";
+    clearTaskContainers();
+});
+
+
+
+
+
